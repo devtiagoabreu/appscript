c4680936a34c194cf0396fcfdb5abbcd532b89e9
devtiagoabreu <devtiagoabreu@gmail.com>
2025-03-01 16:58:28 -0300

    inserir apenas itens que não forma sincronizados

--- sheetsCalendar/code.js
+++ sheetsCalendar/code.js
@@ -1,41 +1,51 @@
 function criarEventos() {
-  var valores = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getDataRange().getValues();
-  Logger.log(valores);
+  
+  var planilha = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  var valores = planilha.getDataRange().getValues();
+  //Logger.log(valores);
 
   var agenda = CalendarApp.getDefaultCalendar();
 
   for (var i = 1; i < valores.lenght; i++) {
     var linha = valores[i];
+    var sincronizado = linha[8];
 
-    var dia = linha[0];
-    var horaInicio = linha[1];
-    var horaFim = linha[2];
-    var titulo = linha[3];
-    var descricao = linha[4];
-    var prioridade = linha[6];
-    
-    var dataInicio = new Date(dia);
-    var dataFim = new Date(dia);
-    dataInicio.setHours(horaInicio.split(":")[0], horaInicio.split(":")[1]);
-    dataFim.setHours(horaFim.split(":")[0], horaFim.split(":")[1]);
+    if ( sincronizado.toLowerCase() !== "Sim") {
 
-    var cor;
-    switch (prioridade) {
-      case "Alta":
-        cor = CalendarApp.EventColor.RED;
-        break;
-      case "Média":
-        cor = CalendarApp.EventColor.YELLOW;
-        break;
-      case "Baixa":
-        cor = CalendarApp.EventColor.GREEN;
-        break;
-      default:
-        cor = CalendarApp.EventColor.BLUE;
-    }
+      var dia = linha[0];
+      var horaInicio = linha[1];
+      var horaFim = linha[2];
+      var titulo = linha[3];
+      var descricao = linha[4];
+      var prioridade = linha[6];
+      
+      var dataInicio = new Date(dia);
+      var dataFim = new Date(dia);
+      dataInicio.setHours(horaInicio.split(":")[0], horaInicio.split(":")[1]);
+      dataFim.setHours(horaFim.split(":")[0], horaFim.split(":")[1]);
 
-    var evento = agenda.createEvent(titulo, dataInicio, dataFim,{description: descricao});
-    evento.setColor(cor);
+      var cor;
+      switch (prioridade) {
+        case "Alta":
+          cor = CalendarApp.EventColor.RED;
+          break;
+        case "Média":
+          cor = CalendarApp.EventColor.YELLOW;
+          break;
+        case "Baixa":
+          cor = CalendarApp.EventColor.GREEN;
+          break;
+        default:
+          cor = CalendarApp.EventColor.BLUE;
+      }
+
+      var evento = agenda.createEvent(titulo, dataInicio, dataFim,{description: descricao});
+      evento.setColor(cor);
+
+      // Atualiza coluna sincronizado
+      panilha.getRange(i + 1, 9).setValue("Sim");
+
+    }    
         
   }
 }
