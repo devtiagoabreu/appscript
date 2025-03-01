70f73a14c2b608700ad701aa0ebe923c58173916
devtiagoabreu <devtiagoabreu@gmail.com>
2025-03-01 12:33:09 -0300

    Chamando a API da openAI

--- ia/Código.js
+++ ia/Código.js
@@ -2,7 +2,41 @@ function chat() {
   var planilha = SpreadsheetApp.getActiveSpreadsheet();
   var aba = planilha.getActiveSheet();
   var prompt = aba.getRange('H2').getValue();
+  
+  //Logger.log('prompt:' + prompt)
+}
 
-  // wow
+function chamarAPI() {
+  var url = 'https://api.openai.com/v1/chat/completions';
+  var prompt = 'Como calcular o valor médio de um estoque contábil';
+
+  var data = {
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant."
+      },
+      {
+        role: "user",
+        content: prompt
+      }
+    ]
+  };
+
+  var options = {
+    method: 'post',
+    contentType: 'application/json',
+    headers: {
+      Authorization: 'Bearer ' + '' // SUA API KEY 
+    },
+    payload: JSON.stringify(data)
+  };
+
+  var response = UrlFetchApp.fetch(url, options);
+  var jsonResponse= response.getContentText();
+
+  return jsonResponse;
+
   //Logger.log('prompt:' + prompt)
 }
