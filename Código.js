636fc3dedaf74ee1066c86951e0c11ce141079d8
devtiagoabreu <devtiagoabreu@gmail.com>
2025-03-01 11:38:00 -0300

    .

--- Código.js
+++
@@ -1,8 +0,0 @@
-function chat() {
-  var planilha = SpreadsheetApp.getActiveSpreadsheet();
-  var aba = planilha.getActiveSheet();
-  var prompt = aba.getRange('H2').getValue();
-
-  
-  //Logger.log('prompt:' + prompt)
-}
